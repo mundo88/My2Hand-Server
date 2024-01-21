e062b8dbe11bbab10a366b97a51a60e7a32ce5d2
mundo88 <minhphung8898@gmail.com>
2024-01-22 06:54:34 +0700

    mundo

---
+++ my2hand/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("stories", views.story, name="stories"),
+    path("posts", views.posts, name="posts"),
+    path("categories", views.categories, name="categories"),
+    path("products", views.products, name="products"),
+    path("users", views.users, name="users"),
+]
