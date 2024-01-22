14ac3f6544d26801ab5cfc29aa9d1297c44b01a1
mundo88 <minhphung8898@gmail.com>
2024-01-23 04:30:58 +0700

    v

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path("stories", views.story, name="stories"),
     path("posts", views.posts, name="posts"),
     path("categories", views.categories, name="categories"),
+    path("categories/<int:category_id>", views.category, name="category"),
     path("products", views.products, name="products"),
+    path("products/<int:product_id>", views.product, name="product"),
     path("users", views.users, name="users"),
+    path("users/<int:user_id>", views.user, name="user"),
+
 ]
